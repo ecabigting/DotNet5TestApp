9eb6ca6e0f8bfc3b8654316a4ddf78b72516b543
ecabigting <ericcabigting@outlook.com>
2021-01-01 14:31:54 +0400

    Repo Init

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Web/RestAPI/TestAPI.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Web.RestAPI
+{
+    [Produces("application/json")]
+    [Route("api/TestAPI")]
+    public class TestAPI : Controller
+    {
+        [HttpGet("ReturnDate")]
+        public IActionResult ReturnDate() 
+        {
+            return CreatedAtAction("ReturnDate", DateTime.Now);
+        }
+    }
+}
